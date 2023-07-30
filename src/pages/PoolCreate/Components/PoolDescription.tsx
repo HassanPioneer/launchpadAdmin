129d91d12ddbcf5299cbe2891c3f692b09d4da36
ChainGPT Dev <125968398+chaingptuser@users.noreply.github.com>
2023-07-30 11:54:43 -0400

    Update PoolDescription.tsx

--- src/pages/PoolCreate/Components/PoolDescription.tsx
+++ src/pages/PoolCreate/Components/PoolDescription.tsx
@@ -1,13 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import useStyles from "../style";
-// @ts-ignore
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-// @ts-ignore
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import CKEditor from 'ckeditor4-react';
 import {renderErrorCreatePool} from "../../../utils/validate";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-
-// CSS in /src/index.css
 
 function PoolDescription(props: any) {
   const classes = useStyles();
@@ -33,44 +27,20 @@ function PoolDescription(props: any) {
       <div className={classes.formCKEditor}>
         <label className={classes.formControlLabel}>About the pool: </label>
 
-        {/*<CKEditor*/}
-        {/*  editor={ ClassicEditor }*/}
-        {/*  data={description}*/}
-        {/*  onReady={ (editor: any) => {*/}
-        {/*    // You can store the "editor" and use when it is needed.*/}
-        {/*    // console.log( 'Editor is ready to use!', editor );*/}
-        {/*  } }*/}
-        {/*  onChange={ ( event: any, editor: any ) => {*/}
-        {/*    const data = editor.getData();*/}
-        {/*    setDescription(data)*/}
-        {/*  } }*/}
-        {/*  onBlur={ ( event: any, editor: any ) => {*/}
-        {/*    // console.log( 'Blur.', editor );*/}
-        {/*  } }*/}
-        {/*  onFocus={ ( event: any, editor: any ) => {*/}
-        {/*    // console.log( 'Focus.', editor );*/}
-        {/*  } }*/}
-        {/*  // disabled={isDeployed}*/}
-        {/*/>*/}
-        {/*<input*/}
-        {/*  type="hidden"*/}
-        {/*  value={description}*/}
-        {/*  name="description"*/}
-        {/*  ref={register({*/}
-        {/*    // required: true*/}
-        {/*  })}*/}
-        {/*/>*/}
+        <CKEditor
+          data={description}
+          onChange={ evt => {
+              const data = evt.editor.getData();
+              setDescription(data);
+          }}
+          readOnly={isDeployed}
+        />
 
-        <textarea
+        <input
+          type="hidden"
           value={description}
           name="description"
-          ref={register({
-          })}
-          onChange={e => setDescription(e?.target?.value)}
-          className={classes.formControlInput}
-          rows={10}
-          cols={50}
-          // maxLength={1000}
+          ref={register}
         />
 
         <p className={classes.formErrorMessage}>
