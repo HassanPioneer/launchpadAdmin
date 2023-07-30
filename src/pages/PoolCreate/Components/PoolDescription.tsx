5bdc9eef753c0a5f00d90abf88f531db149cd865
ChainGPT Dev <125968398+chaingptuser@users.noreply.github.com>
2023-07-30 14:56:42 -0400

    Update PoolDescription.tsx

--- src/pages/PoolCreate/Components/PoolDescription.tsx
+++ src/pages/PoolCreate/Components/PoolDescription.tsx
@@ -31,26 +31,38 @@ function PoolDescription(props: any) {
       <div className={classes.formCKEditor}>
         <label className={classes.formControlLabel}>About the pool: </label>
 
-        <CKEditor
-          editor={ ClassicEditor }
-          data={description}
-          onReady={ (editor: any) => {
-            // You can store the "editor" and use when it is needed.
-            // console.log( 'Editor is ready to use!', editor );
-          } }
-          onChange={ ( event: any, editor: any ) => {
-            const data = editor.getData();
-            setDescription(data);
-            setValue('description', data);  // save the value from the editor into the form
-          } }
-          onBlur={ ( event: any, editor: any ) => {
-            // console.log( 'Blur.', editor );
-          } }
-          onFocus={ ( event: any, editor: any ) => {
-            // console.log( 'Focus.', editor );
-          } }
-          // disabled={isDeployed}
-        />
+<CKEditor
+  editor={ ClassicEditor }
+  data={description}
+  onReady={ (editor: any) => {
+    // You can store the "editor" and use when it is needed.
+    // console.log( 'Editor is ready to use!', editor );
+  } }
+  onChange={ ( event: any, editor: any ) => {
+    const data = editor.getData();
+    setDescription(data);
+    setValue('description', data);  // save the value from the editor into the form
+  } }
+  onBlur={ ( event: any, editor: any ) => {
+    // console.log( 'Blur.', editor );
+  } }
+  onFocus={ ( event: any, editor: any ) => {
+    // console.log( 'Focus.', editor );
+  } }
+  // disabled={isDeployed}
+/>
+
+<input
+  type="hidden"
+  value={description}
+  name="description"
+  ref={register({
+    // required: true
+  })}
+/>
+
+{/* Other form inputs */}
+
 
         <p className={classes.formErrorMessage}>
           {
